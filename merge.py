811fc909cb65c1921a03388a9b13c9362fcf5b71
XinChen1020 <xinc1020@gmail.com>
2024-03-21 19:54:18 -0400

    Even mix of about and nav data for testing

--- merge.py
+++ merge.py
@@ -1,17 +1,23 @@
 import random
 import csv
 
-with open('nav_footers_updated_v2.csv') as f_nav, open('first_dataset_about.csv') as f_about, open('merged_datasets_new.csv', 'w') as f_output, open('nav_footers.csv') as l_nav:
-    _, *d1 = csv.reader(f_nav)
-    _, *d2 = csv.reader(f_about)
-    _, *l1 = csv.reader(l_nav)
+with open('nav_footers_updated_v2.csv') as f_nav, open('about_data_updated.csv') as f_about, open('merged_datasets_new.csv', 'w') as f_output, open('nav_footers.csv') as l_nav:
+    with open('merged_datasets_new_testing.csv', 'w') as test:
+        _, *d1 = csv.reader(f_nav)
+        _, *d2 = csv.reader(f_about)
+        _, *l1 = csv.reader(l_nav)
 
-    merged = d1+ [[a, "", b] for a, b in d2]
+        merged = d1+ [[a, "", b] for a, b in d2]
 
 
-    random.shuffle(merged)
+        write = csv.writer(f_output)
+        write.writerows([['location', 'url', 'text'], *merged])
+        
+        
+        test_merged = d1[:100]+ [[a, "", b] for a, b in d2[:100]]
+        random.shuffle(test_merged)
 
-    write = csv.writer(f_output)
-    write.writerows([['location', 'url', 'text'], *merged])
+        write = csv.writer(test)
+        write.writerows([['location', 'url', 'text'], *test_merged])
     
     

e704f14857a80854821b0c381daaaf61c6f4bd87
XinChen1020 <xinc1020@gmail.com>
2024-03-21 19:31:58 -0400

    merge script and apply shuffling

--- merge.py
+++ merge.py
@@ -1,14 +1,17 @@
 import random
 import csv
 
-with open('first_dataset_nav.csv') as f_nav, open('first_dataset_about.csv') as f_about, open('merged_datasets_new.csv', 'w') as f_output, open('nav_footers.csv') as l_nav:
+with open('nav_footers_updated_v2.csv') as f_nav, open('first_dataset_about.csv') as f_about, open('merged_datasets_new.csv', 'w') as f_output, open('nav_footers.csv') as l_nav:
     _, *d1 = csv.reader(f_nav)
     _, *d2 = csv.reader(f_about)
     _, *l1 = csv.reader(l_nav)
 
+    merged = d1+ [[a, "", b] for a, b in d2]
 
-    merged = [[l[0],d[0], d[-1]] for d, l  in zip(d1,l1)] + [[a, "", b] for a, b in d2]
 
     random.shuffle(merged)
+
     write = csv.writer(f_output)
-    write.writerows([['url', 'location', 'text'], *merged])
+    write.writerows([['location', 'url', 'text'], *merged])
+    
+    
